87cbf93aa04954ee14894b9b002b57f81cc6754d
Adam Manuel <adam@plnar.ai>
2022-03-23 00:53:13 -0500

    added hotjar

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,6 +3,27 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { IconContext } from 'react-icons'
 
 export default class MyDocument extends Document {
+  initHotJar = () => {
+    ;(function (h: any, o: any, t: any, j: any, a, r) {
+      h.hj =
+        h.hj ||
+        function () {
+          // eslint-disable-next-line prefer-rest-params
+          ;(h.hj.q = h.hj.q || []).push(arguments)
+        }
+      h._hjSettings = { hjid: 2275222, hjsv: 6 }
+      a = o.getElementsByTagName('head')[0]
+      r = o.createElement('script')
+      r.async = 1
+      r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv
+      a.appendChild(r)
+    })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=')
+  }
+
+  componentDidMount() {
+    this.initHotJar()
+  }
+
   render() {
     return (
       <IconContext.Provider value={{ style: { verticalAlign: 'middle' } }}>
@@ -10,6 +31,7 @@ export default class MyDocument extends Document {
           <Head>
             <link rel='shortcut icon' href='/avatar.jpeg' />
             <link rel='manifest' href='/manifest.json' />
+            <script>{}</script>
           </Head>
           <body>
             <script src='noflash.js' />
